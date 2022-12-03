c5807f1bf9f99c221854de682c96cfa38c35fbc8
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-03 14:17:12 +0100

    Added logic of checking win condition

--- src/day2/Part1.java
+++ src/day2/Part1.java
@@ -9,6 +9,29 @@ public class Part1 {
 	public static void main(String args[]) throws FileNotFoundException {
 		File file = new File("rock-paper-scissors.txt");
 		Scanner text = new Scanner(file);
+		
+		while(text.hasNextLine()) {
+			String read;
+			char player, opponent;
+			read = text.nextLine();
+			opponent = read.charAt(0);
+			player = read.charAt(2); 
+			System.out.print(opponent + " vs " + player);
+			switch (player - opponent) {
+				case 23:
+					System.out.println(" Remis");
+					break;
+				case 24:
+				case 21:
+					System.out.println(" Wygrana");
+					break;
+				case 22:
+				case 25:
+					System.out.println(" Pora¿ka");
+					break;
+			}
+		}
+		
 		text.close();
 		
 	}
