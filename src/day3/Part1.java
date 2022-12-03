d4dc05269da3e9b725978a8439283ae25f6b7d93
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-03 15:06:59 +0100

    Implemented a method to find common letter in two equal parts of a given string

--- src/day3/Part1.java
+++ src/day3/Part1.java
@@ -6,14 +6,33 @@ import java.util.Scanner;
 
 public class Part1 {
 	public static void main(String args[]) throws FileNotFoundException {
-		
-		File file = new File("inputs\rucksack.txt");
+
+		File file = new File("inputs\\rucksack.txt");
 		Scanner text = new Scanner(file);
-		
-		while(text.hasNextLine()) {
-			
+		String read;
+
+		while (text.hasNextLine()) {
+			read = text.nextLine();
+			char common = findCommon(read);
+			System.out.println(read + " : " + common);
 		}
-		
-		
+		text.close();
+	}
+	
+	
+	public static char findCommon(String rucksack) {
+		int middle = rucksack.length() / 2;
+		char common = ' ';
+		String[] rucksackParts = { rucksack.substring(0, middle), rucksack.substring(middle) };
+
+		for (char i : rucksackParts[0].toCharArray()) {
+			String check = Character.toString(i);
+			if (rucksackParts[1].contains(check)) {
+				common = i;
+				break;
+			}
+
+		}
+		return common;
 	}
 }
