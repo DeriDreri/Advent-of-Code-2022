c19f6326cddab92db51e49497f5feee51937c882
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-03 13:20:42 +0100

    First solution for the task

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -1,5 +1,34 @@
 package day1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Day1 {
+
+	public static void main(String args[]) throws FileNotFoundException {
+
+		int max = 0;
+		int current = 0;
+		String read;
+
+		File file = new File("calories.txt");
+		Scanner text = new Scanner(file);
+
+		while (text.hasNextLine()) {
+			read = text.nextLine();
+
+			if (read != "")
+				current = +Integer.parseInt(read);
+
+			else {
+				if (current > max)
+					max = current;
+				current = 0;
+			}
+		}
+
+		System.out.println(max);
+	}
 
 }
