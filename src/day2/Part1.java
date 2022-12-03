96ddb2183224290fd43b6216bc05dc84280168a0
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-03 14:22:10 +0100

    Finished the first part of the day 2

--- src/day2/Part1.java
+++ src/day2/Part1.java
@@ -5,35 +5,39 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Part1 {
-	
+
 	public static void main(String args[]) throws FileNotFoundException {
 		File file = new File("rock-paper-scissors.txt");
 		Scanner text = new Scanner(file);
 		
-		while(text.hasNextLine()) {
+		int points = 0;
+		
+		while (text.hasNextLine()) {
 			String read;
 			char player, opponent;
+
 			read = text.nextLine();
 			opponent = read.charAt(0);
-			player = read.charAt(2); 
-			System.out.print(opponent + " vs " + player);
+			player = read.charAt(2);
+			
+			points += player - 87;
+			
 			switch (player - opponent) {
-				case 23:
-					System.out.println(" Remis");
-					break;
-				case 24:
-				case 21:
-					System.out.println(" Wygrana");
-					break;
-				case 22:
-				case 25:
-					System.out.println(" Pora¿ka");
-					break;
+			case 23:
+				points += 3;
+				break;
+			case 24:
+			case 21:
+				points += 6;
+				break;
+			case 22:
+			case 25:
+				break;
 			}
 		}
-		
+		System.out.println(points);
 		text.close();
-		
+
 	}
 
 }
