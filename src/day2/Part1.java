882b226a23a6e9f5b57a13b92f764912546bbcf2
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-03 14:03:15 +0100

    Initialised file reader

--- src/day2/Part1.java
+++ src/day2/Part1.java
@@ -1,8 +1,15 @@
 package day2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part1 {
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws FileNotFoundException {
+		File file = new File("rock-paper-scissors.txt");
+		Scanner text = new Scanner(file);
+		text.close();
 		
 	}
 
