6752ff06b17573ea016fde8f2f2259d7a87dc40a
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-03 14:46:19 +0100

    Initalised Part 1 of Day 3, imported input file

---
+++ src/day3/Part1.java
@@ -0,0 +1,19 @@
+package day3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Part1 {
+	public static void main(String args[]) throws FileNotFoundException {
+		
+		File file = new File("rucksack.txt");
+		Scanner text = new Scanner(file);
+		
+		while(text.hasNextLine()) {
+			
+		}
+		
+		
+	}
+}
