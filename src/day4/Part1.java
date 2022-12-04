883715d6e4f79a28934393ba9faf83aea3d9d9ce
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-04 15:38:48 +0100

    Implemented file access and the main loop of reading datra?

--- src/day4/Part1.java
+++ src/day4/Part1.java
@@ -1,5 +1,25 @@
 package day4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part1 {
 
+	public static void main(String args[]) throws FileNotFoundException {
+		
+		File file = new File("inputs\\cleaning.txt");
+		Scanner text = new Scanner(file);
+		
+		
+		while(text.hasNextLine()) {
+			String read = text.nextLine();
+			
+			
+			
+		}
+		
+		text.close();
+	}
+	
 }
