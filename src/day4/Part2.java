2e45b209c14a8e678e909388f3da7be5ba44ac3c
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-04 15:59:17 +0100

    Initaiated the Part 2 of Day 4

--- src/day4/Part2.java
+++ src/day4/Part2.java
@@ -1,5 +1,23 @@
 package day4;
 
-public class Part2 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Part2 {
+	g
+	public static void main(String args[]) throws FileNotFoundException {
+		File file = new File("inputs\\cleaning.txt");
+		Scanner text = new Scanner(file);
+
+		while (text.hasNextLine()) {
+			String read = text.nextLine();
+
+			String[] elves = read.split(",");
+			String[] first = elves[0].split("-");
+			String[] second = elves[1].split("-");
+		}
+
+		text.close();
+	}
 }
