71583553b039f467a0d69bd682a5c0f677a28bec
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-05 15:18:03 +0100

    Added file access

--- src/day5/Part1.java
+++ src/day5/Part1.java
@@ -1,5 +1,15 @@
 package day5;
 
-public class Part1 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Part1 {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("inputs\\cleaning.txt");
+		Scanner text = new Scanner(file);
+		
+		
+	}
 }
