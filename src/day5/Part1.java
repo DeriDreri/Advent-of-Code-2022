5fa374454af63ac463f6abe19c1536b698d60d6f
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-05 15:53:09 +0100

    Finished reading all data from input, prepared for operation on the crates

--- src/day5/Part1.java
+++ src/day5/Part1.java
@@ -2,36 +2,53 @@ package day5;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Part1 {
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		File file = new File("inputs\\crates.txt");
 		Scanner text = new Scanner(file);
-		boolean initialScan = true;
-		LinkedList<char[]> crates = new LinkedList<char[]>();
-		while(true) {
+
+		LinkedList<char[]> cratesBuff = new LinkedList<char[]>();
+		while (true) {
 			String read = text.nextLine();
 			char[] cratesRead = new char[9];
-			if (read.charAt(1) == '1') break;
+			if (read.charAt(1) == '1')
+				break;
 			for (int i = 0; i < 9; i++) {
 				cratesRead[i] = read.charAt(1 + 4 * i);
 			}
-			crates.add(cratesRead);
+			cratesBuff.add(cratesRead);
 		}
 		text.nextLine();
-		
-		for(char[] i : crates) {
-			for(char j : i) {
-				System.out.print("[" + j + "] ");
+
+		ArrayList<LinkedList<Character>> crates = new ArrayList<LinkedList<Character>>();
+		for (int i = 0; i < 9; i++)
+			crates.add(new LinkedList<Character>());
+		for (int i = cratesBuff.size() - 1; i >= 0; i--) {
+			char[] split = cratesBuff.get(i);
+			for (int j = 0; j < split.length; j++) {
+				crates.get(j).add(split[j]);
 			}
-			System.out.println();
 		}
-		/*while(text.hasNextLine()) {
+		
+
+		while (text.hasNextLine()) {
+			String instrRaw = text.nextLine(); 
+			String[] instrPart1 = instrRaw.split(" from "); 
+			String[] instrPart2 = instrPart1[1].split(" to ");
+			int[] inst = {Integer.parseInt(instrPart1[0].substring(5)), Integer.parseInt(instrPart2[0]),Integer.parseInt(instrPart2[1])};
+	
 			
-		}*/
+		}
+
 		text.close();
+		
+		  for (LinkedList<Character> i : crates) { for (Character j : i) {
+		  System.out.print(j); } System.out.println(); }
+		 
 	}
 }
