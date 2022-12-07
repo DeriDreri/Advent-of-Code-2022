372218c14eb5bda3fdc5ef39abceb63f1e9331fc
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-07 20:43:08 +0100

    Added file reader to both Part 1 and Part2 classes

--- src/day7/Part1.java
+++ src/day7/Part1.java
@@ -1,5 +1,17 @@
 package day7;
 
-public class Part1 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Part1 {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("inputs\\files.txt");
+		Scanner text = new Scanner(file);
+		
+		while(text.hasNextLine()) {
+			String read = text.nextLine();
+		}
+	}
 }
--- src/day7/Part2.java
+++ src/day7/Part2.java
@@ -1,5 +1,19 @@
 package day7;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part2 {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("inputs\\files.txt");
+		Scanner text = new Scanner(file);
+		
+		while(text.hasNextLine()) {
+			String read = text.nextLine();
+		}
+	}
+
 
 }
