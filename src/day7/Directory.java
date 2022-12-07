4638bebcd9ceaa7ebca74502eddba73e552b7837
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-07 20:38:59 +0100

    Created a class representing directory in the file system

---
+++ src/day7/Directory.java
@@ -0,0 +1,42 @@
+package day7;
+
+import java.util.LinkedList;
+
+public class Directory {
+
+	private LinkedList<String> files;
+	private String name;
+	private Directory parent;
+	private int size; 
+	
+	Directory(String name, Directory parent) {
+		this.files = new LinkedList<String>();
+		this.name = name;
+		this.parent = parent;
+		this.size = 0;
+	}
+
+	public void addFile(String name, int size) {
+		for (String i : files) {
+			if (i == name)
+				return;
+		}
+		files.add(name);
+		this.size += size;
+	}
+	
+	public int getSize() {
+		return size;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Directory getParent() {
+		if (parent == null) return this;
+		return parent;
+	}
+
+}
