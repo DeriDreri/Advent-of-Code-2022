181913b76ef6463aa436a412e5b77ab0ed4c4eec
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-08 15:05:00 +0100

    Created classes for Day 8, imported input

---
+++ src/day8/Part2.java
@@ -0,0 +1,14 @@
+package day8;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Part2 {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("inputs\\files.txt");
+		Scanner text = new Scanner(file);
+	}
+
+}
