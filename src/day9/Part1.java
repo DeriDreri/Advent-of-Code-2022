8e3873a666b931b24a7a54a9513892017a2336fd
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-09 11:02:53 +0100

    Implemented data reading mechanism and tested it

--- src/day9/Part1.java
+++ src/day9/Part1.java
@@ -1,5 +1,29 @@
 package day9;
 
+import java.awt.Point;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part1 {
+	
+	public static void main(String args[]) throws FileNotFoundException {
+		File file = new File("inputs\\rope.txt");
+		Scanner text = new Scanner(file);
+		
+		Point headKnot = new Point();
+		Point tailKnot = new Point();
+		
+		while(text.hasNextLine()) {
+			String read = text.nextLine();
+			String directionOfMotion = read.substring(0,1);
+			String periodOfMotion = read.substring(2);
+			
+			System.out.println("Move " + directionOfMotion + " for " + periodOfMotion);
+			
+		}
+		
+		
+	}
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,4 @@
 module main {
 	requires java.base;
+	requires java.desktop;
 }
