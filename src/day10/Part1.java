880f1f2c9e2f08d7456c86f49ccf058e458d1477
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-10 18:39:21 +0100

    Added the correct input format, solved Part 1

--- src/day10/Part1.java
+++ src/day10/Part1.java
@@ -1,5 +1,30 @@
 package day10;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part1 {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		var file = new File("inputs//cycles.txt");
+		var text = new Scanner(file);
+		
+		int registerX = 1;
+		int cycleNumber = 0;
+		int sumOfRegisters = 0;
+		
+		while(text.hasNextLine()) {
+			cycleNumber++;
+			if(cycleNumber % 40 == 20) sumOfRegisters += registerX * cycleNumber;
+			String[] command = text.nextLine().split(" ");
+			if (command[0].equals("noop")) continue;
+			cycleNumber++;
+			if(cycleNumber % 40 == 20) sumOfRegisters += registerX * cycleNumber;
+			registerX += Integer.parseInt(command[1]);
+		}
+		
+		System.out.println(sumOfRegisters);
+	}
 
 }
