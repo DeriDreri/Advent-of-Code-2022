884ddbe0756678c1a40da256817e9751574a7d01
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-10 18:57:14 +0100

    Implemented solution for Task 2 of Day 10

--- src/day10/Part2.java
+++ src/day10/Part2.java
@@ -1,5 +1,40 @@
 package day10;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part2 {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		var file = new File("inputs//cycles.txt");
+		var text = new Scanner(file);
+		
+		int registerX = 1;
+		int cycleNumber = 0;
+		int sumOfRegisters = 0;
+		
+		
+		
+		while(text.hasNextLine()) {
+			cycleNumber = executeCycle(cycleNumber, registerX);
+			String[] command = text.nextLine().split(" ");
+			if (command[0].equals("noop")) continue;
+			cycleNumber = executeCycle(cycleNumber, registerX);
+			registerX += Integer.parseInt(command[1]);
+		}
+	}
+	
+	public static int executeCycle(int cycleNumber, int positionX) {
+		cycleNumber++;
+		char toPrint = '#';
+		if(cycleNumber < positionX || cycleNumber > positionX+2) toPrint = '.';
+		System.out.print(toPrint);
+		if(cycleNumber % 40 == 0) {
+			cycleNumber = 0;
+			System.out.println();
+			}
+		return cycleNumber;
+	}
 
 }
