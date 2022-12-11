5ade3eb658f554b44cb5f681469a59600fc17d2c
Dominik Cybulski <dominikcybulski@protonmail.com>
2022-12-11 17:05:52 +0100

    Created Monkey class and CalculateWorryLevel interface

---
+++ src/day11/CalculateWorryLevel.java
@@ -0,0 +1,7 @@
+package day11;
+
+public interface CalculateWorryLevel {
+	
+	public int calculate();
+
+}
---
+++ src/day11/Monkey.java
@@ -0,0 +1,46 @@
+package day11;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Monkey {
+
+	
+	private LinkedList<Integer> items;
+	private CalculateWorryLevel worryLevel;
+	private int test;
+	private int positiveMonkey;
+	private int negativeMonkey;
+	private ArrayList<Monkey> monkeys;
+	private int inspectionsAmount;
+
+	Monkey( LinkedList<Integer> items, CalculateWorryLevel worryLevel, int test, int positiveMonkey, int negativeMonkey, ArrayList<Monkey> monkeys){
+		this.worryLevel = worryLevel;
+		this.items = items;
+		this.positiveMonkey = positiveMonkey;
+		this.negativeMonkey = negativeMonkey;
+		this.inspectionsAmount = 0;
+	}
+	
+	public int getInspectionsAmount() {
+		return this.inspectionsAmount;
+	}
+	
+	public void giveItem(Integer item) {
+		this.items.add(item);
+	}
+	
+	public void inspectItems() {
+		for(Integer item : items) {
+			item = worryLevel.calculate();
+			item /= 3;
+			if(item % test == 0) {
+				this.monkeys.get(positiveMonkey).giveItem(item);
+			}
+			else {
+				this.monkeys.get(negativeMonkey).giveItem(item);
+			}
+		}
+		this.items.clear();
+	}
+}
